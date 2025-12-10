635f1f86aa9e009c1f4dc0f52a01bbc286bd62cc
RituJoshi26 <ritujoshi621@gmail.com>
2025-12-10 12:36:51 +0530

    Added api.js path

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",  // <-- IMPORTANT CHANGE
+  baseURL: "https://mern-landing-admin-backend.onrender.com/api"
 });
 
 export default API;
